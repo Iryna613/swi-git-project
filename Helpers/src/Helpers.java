8ed321e156759ea530dc919d43b902f794dac0db
Iryna613 <tokmynairyna05@gmail.com>
2025-11-27 12:30:42 +0100

    added Helpers func

---
+++ Helpers/src/Helpers.java
@@ -0,0 +1,13 @@
+public class Helpers {
+    public static int square(int x) {
+        return x * x;
+    }
+
+    public static void printWelcomeMessage() {
+        System.out.printf("Hello and welcome!");
+    }
+
+    public static void printRebaseMessage(int i) {
+        System.out.println("try git rebase: " + i);
+    }
+}
